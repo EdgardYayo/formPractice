912612e4e771f935a01d2bf0949ade4987c28f7c
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-03-03 18:03:49 -0600

    Practicing forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'form-practice';
+  @ViewChild('form') submitForm?: NgForm;
+  defaultSub = 'advanced'
+  user = {
+    email: '',
+    sub: '',
+    password: ''
+  }
+
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.user = {
+      email: this.submitForm?.value.email,
+      sub: this.submitForm?.value.subscription,
+      password: this.submitForm?.value.password
+    }
+
+    this.submitForm?.reset()
+  }
 }
